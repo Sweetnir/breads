e5e97e54cace2c2c4ad5599725158da4fa5ead17
Damon Herrera <bustinbubs@gmail.com>
2023-04-22 09:58:54 -0700

    catching up from last class

--- server.js
+++ server.js
@@ -6,6 +6,13 @@ require('dotenv').config()
 const PORT = process.env.PORT
 const app = express()
 
+// MIDDLEWARE
+app.set('views', __dirname + '/views')
+app.set('view engine', 'jsx')
+app.engine('jsx', require('express-react-views').createEngine())
+app.use(express.static('public'))
+app.use(express.urlencoded({extended: true}))
+
 // ROUTES
 app.get('/', (req, res) => {
     res.send('Welcome to an Awesome App about Breads')
@@ -14,8 +21,13 @@ app.get('/', (req, res) => {
   // Breads
   const breadsController = require('./controllers/breads_controller.js')
   app.use('/breads', breadsController)
-  
 
+  // 404 Page
+app.get('*', (req, res) => {
+  res.render('error404')
+})
+
+  
 // LISTEN
 app.listen(PORT, () => {
   console.log('listening on port', PORT);
